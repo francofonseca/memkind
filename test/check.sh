7549314fcb0b549f0bf49a0649f61b98135cf9c9
rosalesf <jose.francisco.rosales.fonseca@intel.com>
2017-05-16 15:18:45 +0100

    Disable tests

--- test/check.sh
+++ test/check.sh
@@ -50,10 +50,6 @@ if [[ $ret == "" ]]; then
         params=$params" -d"
 fi
 
-echo "----------------------- TESTS -----------------------"
-echo $DISABLE_TESTS
-echo "----------------------- TESTS -----------------------"
-
 if [[ -n $DISABLE_TESTS ]]; then
         echo "On demand test disabling detected!"
         params="$params -x $DISABLE_TESTS"
