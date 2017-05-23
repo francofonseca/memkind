03fd2c479c24d0ad70abf3bc9a24595b60ebd6d3
rosalesf <jose.francisco.rosales.fonseca@intel.com>
2017-05-23 20:59:42 +0100

    Disable tests

--- test/test.sh
+++ test/test.sh
@@ -232,7 +232,7 @@ if [[ $ret == "" ]]; then
     TEST_PREFIX="numactl --membind=0 %s"
 fi
 # Execute getopt
-ARGS=$(getopt -o T:c:f:l:hdmgx: -- "$@");
+ARGS=$(getopt -o T:c:f:l:hdmgx:p: -- "$@");
 
 #Bad arguments
 if [ $? -ne 0 ];
